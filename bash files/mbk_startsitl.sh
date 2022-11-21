bac0d9af4eefb8da7ae779a7baacf9ed107441b1
Muhammad Bilal Kadri <bilal.kadri@gmail.com>
2022-11-21 14:50:45 +0500

    Adding bash file

---
+++ bash files/mbk_startsitl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#cd ~/ardupilot/ArduCopter/ && sim_vehicle.py --vehicle=ArduCopter --frame=hexa --console
+#cd ~/ardupilot/ArduCopter/ && sim_vehicle.py -v ArduCopter -f hexa --console
+cd ~/ardupilot/ArduCopter/ && sim_vehicle.py -v ArduCopter -f gazebo-iris --console
+#cd ~/ardupilot/ArduCopter/ && sim_vehicle.py -v ArduCopter -f gazebo-iris --console
